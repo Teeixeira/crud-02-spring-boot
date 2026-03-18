936bc0177b63eb394e77f8bf5aaf93757a07f6d4
Leo Teixeira <167496565+Teeixeira@users.noreply.github.com>
2026-03-18 12:44:19 -0300

    add: controllers

---
+++ src/main/java/com/exercicio/crud_02/controllers/ProdutoController.java
@@ -0,0 +1,55 @@
+package com.exercicio.crud_02.controllers;
+
+
+import com.exercicio.crud_02.models.ProdutoModel;
+import com.exercicio.crud_02.services.ProdutoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/produtos")
+public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoModel> save(@RequestBody ProdutoModel produtoModel){
+        ProdutoModel produto = produtoService.save(produtoModel);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(produtoModel.getId())
+                .toUri();
+
+        return ResponseEntity.ok().body(produto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoModel>> findAll(){
+        List<ProdutoModel> produtos = produtoService.findAll();
+
+        return ResponseEntity.ok().body(produtos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoModel> findById(@PathVariable Long id){
+        ProdutoModel produto = produtoService.findById(id);
+
+        return ResponseEntity.ok().body(produto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        produtoService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/com/exercicio/crud_02/models/ProdutoModel.java
+++ src/main/java/com/exercicio/crud_02/models/ProdutoModel.java
@@ -1,6 +1,7 @@
 package com.exercicio.crud_02.models;
 
 import jakarta.persistence.*;
+import org.jspecify.annotations.Nullable;
 
 import java.math.BigDecimal;
 
@@ -16,4 +17,43 @@ public class ProdutoModel {
     @Column(name = "preco")
     private BigDecimal preco;
     private Integer estoque;
+
+    public ProdutoModel(Long id, String nome, BigDecimal preco, Integer estoque) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
 }
