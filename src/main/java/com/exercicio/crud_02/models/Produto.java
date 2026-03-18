153b6dc1d5990168357387b3c1772c462528161a
Leo Teixeira <167496565+Teeixeira@users.noreply.github.com>
2026-03-18 12:19:00 -0300

    add: models

---
+++ src/main/java/com/exercicio/crud_02/models/Produto.java
@@ -0,0 +1,19 @@
+package com.exercicio.crud_02.models;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tp_produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "descritivo")
+    private String nome;
+    @Column(name = "preco")
+    private BigDecimal preco;
+    private Integer estoque;
+}
